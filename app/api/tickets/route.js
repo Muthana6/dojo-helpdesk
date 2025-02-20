686c0983e6b12635feefa39268f801b1ac41c8ee
Muthana6 <marvelge009@gmail.com>
2025-02-20 13:54:01 +0100

    v13-v15 change problem

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,7 +1,13 @@
 import {NextResponse} from "next/server";
 
+// METHOD 2 works on the whole route
+export const dynamic = 'force-static'
 export async function GET(){
-    const res = await fetch(`http://localhost:4000/tickets`)
+    const res = await fetch(`http://localhost:4000/tickets`
+    //     METHOD 2 works locally on the route
+    //     {next: {revalidate: 0}}
+    )
+
     const tickets = await res.json()
     return NextResponse.json(tickets, {status: 200})
 }
